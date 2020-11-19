abdacca10e5edc4072f138f7fa2f98c923c29ca5
WideanNagari <Wideannagari@gmail.com>
2020-11-19 22:29:50 +0700

    first

---
+++ TyperZombies/Asset.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TyperZombies
+{
+    public class Asset
+    {
+        List<Image> arrZombie1_W;
+        List<Image> arrZombie1_R;
+        List<Image> arrZombie1_D;
+
+        List<Image> arrZombie2_W;
+        List<Image> arrZombie2_R;
+        List<Image> arrZombie2_D;
+
+        List<Image> arrZombie3_W;
+        List<Image> arrZombie3_R;
+        List<Image> arrZombie3_D;
+
+        List<Image> arrZombie4_W;
+        List<Image> arrZombie4_R;
+        List<Image> arrZombie4_D;
+    }
+}
---
+++ TyperZombies/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TyperZombies
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.TextAlign = HorizontalAlignment.Center;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(Image.FromFile("background.png"), 0, 0, 1120, 600);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString().Equals("Space"))
+            {
+                textBox1.Text = "";
+            }
+        }
+    }
+}
